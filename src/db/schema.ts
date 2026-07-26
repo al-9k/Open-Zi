8233791da00579a73f4c1e967c993ddb25bbe4d4
Alhasan <141469172+al-9k@users.noreply.github.com>
2026-07-26 19:17:03 -0400

    Drizzle Postgres Schema Written

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,19 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const cedict_entries = pgTable("cedict", {
+  entry: text().primaryKey(),
+  pinyin: text().notNull(),
+  definition: text().notNull(),
+  
+  // Optional columns (no .notNull())
+  hsk: integer(),
+  frequency: integer(),
+  frequency_rank: integer(),
+  char_rank: integer(),
+});
+
+export const char_bank = pgTable("char_bank", {
+  char: text().primaryKey().references(() => cedict_entries.entry),
+  date_added: text().notNull(),       // ISO timestamp
+  mastered: integer().default(0),     // 0 = learning, 1 = learnt
 });
